9c2e08432505c4e41ccae33ecf9eca524e373e6b
serka <serka@DESKTOP-U0RHS0Q>
2023-03-24 23:04:43 +0300

    seri

--- BusinessLayer/ValidationRules/FacultyValidator.cs
+++ BusinessLayer/ValidationRules/FacultyValidator.cs
@@ -13,11 +13,13 @@ namespace BusinessLayer.ValidationRules
         public FacultyValidator()
         {
             RuleFor(x => x.FacultyName).NotEmpty().WithMessage("Fakülte ismini boş geçemezsiniz.");
-            RuleFor(x => x.FacultyName).MinimumLength(12).WithMessage("15 Karakterden düşük değer girilemez.");
+            RuleFor(x => x.FacultyName).MinimumLength(12).WithMessage("10 Karakterden düşük değer girilemez.");
             RuleFor(x => x.FacultyName).MaximumLength(50).WithMessage("50 Karakterden fazla değer girilemez.");
             RuleFor(x => x.FacultyAddress).NotEmpty().WithMessage("Fakülte adresini boş geçemezsiniz.");
             RuleFor(x => x.FacultyAddress).MinimumLength(12).WithMessage("15 Karakterden düşük değer girilemez.");
             RuleFor(x => x.FacultyAddress).MaximumLength(50).WithMessage("100 Karakterden fazla değer girilemez.");
+            RuleFor(x => x.FacultyStatus).NotEmpty().WithMessage("Fakülte durumu True veya False seçeneklerinden biri olmalıdır. True Aktif , False Pasif Anlamına Gelmektedir");
+            RuleFor(x => x.FacultyCreationDate).NotEmpty().WithMessage("Fakülte kuruluş tarihini boş geçemezsiniz. Kabul edilen format gg.aa.yyyy olmalıdır.");
         }
 
     }
---
+++ InteraktifBilgiEkranı/Controllers/FacultyController.cs
@@ -0,0 +1,71 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InteraktifBilgiEkranı.Controllers
+{
+    public class FacultyController : Controller
+    {
+        // GET: Faculty
+        FacultyManager Fm = new FacultyManager(new EfFacultyDAL());
+        public ActionResult Index()
+        {
+            var facultyValues = Fm.GetList();
+            return View(facultyValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddFaculty()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddFaculty(Faculty p)
+        {
+            FacultyValidator facultyValidator = new FacultyValidator();
+            ValidationResult results = facultyValidator.Validate(p);
+            if (results.IsValid)
+            {
+                Fm.FacultyAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult DeleteFaculty(int id)
+        {
+            var facultyValues = Fm.GetByID(id);
+            Fm.FacultyDelete(facultyValues);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditFaculty(int id)
+        {
+            var facultyValues = Fm.GetByID(id);
+            return View(facultyValues);
+        }
+
+        [HttpPost]
+        public ActionResult EditFaculty(Faculty p)
+        {
+            Fm.FacultyUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
