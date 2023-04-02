5f50702adc65fc58c209c94fc3a272a234344081
serka <serka@DESKTOP-U0RHS0Q>
2023-04-03 00:05:14 +0300

    kukukukuu

--- InteraktifBilgiEkranı/Controllers/LoginController.cs
+++ InteraktifBilgiEkranı/Controllers/LoginController.cs
@@ -23,65 +23,79 @@ namespace InteraktifBilgiEkranı.Controllers
         public ActionResult Index(User p)
         {
             Context c = new Context();
-            var infos = c.Users.FirstOrDefault(x => x.UserMail == p.UserMail &&
-            x.UserPassword == p.UserPassword);
-            
-            if (infos != null)
-            {
-                FormsAuthentication.SetAuthCookie(infos.UserMail,false);
+            var infos = c.Users.Where(x => x.UserMail == p.UserMail).SingleOrDefault();
+            if(infos.UserMail==p.UserMail && infos.UserPassword == Crypto.Hash(p.UserPassword,"MD5")){
+
+                FormsAuthentication.SetAuthCookie(infos.UserMail, false);
                 Session["UserMail"] = infos.UserMail;
                 return RedirectToAction("Index", "About");
             }
             else
             {
+                //ViewBag.uyarı = "Kullanıcı adı veya Şifre Yanlış";
                 return RedirectToAction("Index");
             }
+            
+            //Context c = new Context();
+            //var infos = c.Users.FirstOrDefault(x => x.UserMail == p.UserMail &&
+            //x.UserPassword == p.UserPassword);
+
+            //if (infos != null)
+            //{
+            //    FormsAuthentication.SetAuthCookie(infos.UserMail,false);
+            //    Session["UserMail"] = infos.UserMail;
+            //    return RedirectToAction("Index", "About");
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Index");
+            //}
         }
-        //[HttpGet]
-        //public ActionResult ForgotMyPassword()
-        //{
-        //    return View();
 
-        //}
+        [HttpGet]
+        public ActionResult ForgotMyPassword()
+        {
+            return View();
 
-        //[HttpPost]
-        //public ActionResult ForgotMyPassword(string usermail)
-        //{
-        //    Context c = new Context();
-        //    var mail = c.Users.Where(x => x.UserMail == usermail).SingleOrDefault();
+        }
 
-        //    if (mail != null)
-        //    {
-        //        Random rnd = new Random();
-        //        int newpassword = rnd.Next();
+        [HttpPost]
+        public ActionResult ForgotMyPassword(string usermail)
+        {
+            Context c = new Context();
+            var mail = c.Users.Where(x => x.UserMail == usermail).SingleOrDefault();
 
-        //        User user = new User();
-        //        mail.UserPassword = Convert.ToString(newpassword);
-        //        c.SaveChanges();
+            if (mail != null)
+            {
+                Random rnd = new Random();
+                int newpassword = rnd.Next();
 
+                User user = new User();
+                mail.UserPassword = Crypto.Hash(Convert.ToString(newpassword), "MD5");
+                c.SaveChanges();
 
-        //        WebMail.SmtpServer = "smtp.gmail.com";
-        //        WebMail.EnableSsl = true;
-        //        WebMail.UserName = "baskentbilgiekrani@gmail.com";
-        //        WebMail.Password = "ntxrkkvzavktxqqe";
-        //        WebMail.SmtpPort = 587;
-        //        WebMail.Send(usermail, "Admin Panel Giriş Şifreniz", " şifreniz:" + newpassword);
-        //        ViewBag.Uyari = " Mesajınız Başarı ile gönderilmiştir.";
-        //    }
-        //    else
-        //    {
-        //        ViewBag.Uyari = " Hata oluştu. Tekrar Deneyiniz:";
-        //    }
-        //    return View();
-        //}
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "baskentbilgiekrani@gmail.com";
+                WebMail.Password = "ntxrkkvzavktxqqe";
+                WebMail.SmtpPort = 587;
+                WebMail.Send(usermail, "Şifre Sıfırlama İşlemi", "Merhaba " + mail.UserName + mail.UserSurname+"," + " <br/> yeni şifreniz: " + newpassword + " olarak belirlenmiştir." + "<br/> İyi günler dileriz.");
+                ViewBag.Uyari = " Mesajınız Başarı ile gönderilmiştir.";
+            }
+            else
+            {
+                ViewBag.Uyari = " Hata oluştu. Tekrar Deneyiniz:";
+            }
+            return View();
+        }
 
-        //public ActionResult LogOut()
-        //{
-        //    Session["UserID"] = null;
-        //    Session["UserMail"] = null;
-        //    Session.Abandon();
-        //    return RedirectToAction("Login", "Index");
-        //}
+        public ActionResult LogOut()
+        {
+            //Session["UserID"] = null;
+            Session["UserMail"] = null;
+            Session.Abandon();
+            return RedirectToAction("Login", "Index");
+        }
 
     }
 }
